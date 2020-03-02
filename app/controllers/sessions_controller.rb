45c962ec4fb94df15a3526238f22b8059273091d
corn-cloud <comowaiye@gmail.com>
2020-03-02 17:57:40 -0600

    update

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,19 @@
 class SessionsController < ApplicationController
   def new
+    @user = User.new
   end
+
+  def create
+    user = User.find_by(name: params[:user][:name])
+    user = User.try(:authenticate, params[:user][:password])
+    if user 
+      session[:user_id] = user.id
+      @user = user
+      redirect_to '/user/welcome'
+    else 
+      redirect_to '/sessions/new'
+    end
+  end
+
+  
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+
+  
   def index
   end
 
@@ -6,8 +8,16 @@ class UsersController < ApplicationController
   end
 
   def show
+    @user = User.find(params[:id])
   end
 
   def edit
   end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :nick_name, :password_digest)
+  end
+
 end
