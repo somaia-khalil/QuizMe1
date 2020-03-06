f6f986c37233fc9fbc5fa79a2efe276ca621ffee
Somaia Khalil <sumaya_khair@live.com>
2020-03-06 09:14:06 +0100

    added collaps card layout and teacher stats page

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -2,4 +2,7 @@ class Answer < ApplicationRecord
     belongs_to :question
     has_many :results
     has_many :participants , through: :results
+
+    validates :points, presence: true
+    validates :answer_text, presence: true, :length => {:minimum => 1}
 end
--- app/models/result.rb
+++ app/models/result.rb
@@ -2,4 +2,32 @@ class Result < ApplicationRecord
     belongs_to :answer
     belongs_to :question
     belongs_to :participant
+
+
+  def self.participant_results(participant)
+    # give all results a participant scored at any quiz
+    self.where(participant_id: participant.id)
+  end 
+
+  def self.quiz_results(participant, quiz)
+    # gives a list of all results a participant scored at a certain quiz
+    participant_results(participant).where(question_id: quiz.question_ids)
+  end
+
+  def self.quiz_score(participant, quiz)
+    # calculates the total score the user achieved in a certain quiz
+    quiz_results(participant, quiz).sum do |result|
+      result.answer.points
+    end
+  end
+
+  def self.quiz_score(participant, quiz)
+    # calculates the total score the user achieved in a certain quiz
+    quiz_results(participant, quiz).sum do |result|
+      result.answer.points
+    end
+  end
+
+
+
 end
